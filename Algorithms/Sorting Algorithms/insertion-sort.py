677e6358458a02f570ae7024f9184fdfc8259362
Zelalem <leoxanigm@gmail.com>
2022-09-06 14:42:57 +0200

    add insertion sort

--- Algorithms/Sorting Algorithms/insertion-sort.py
+++ Algorithms/Sorting Algorithms/insertion-sort.py
@@ -1,4 +1,18 @@
-from swap import swap
+def insertion_sort(A):
+    '''
+    Works by picking the next number in the array and picking a new
+    position to insert it
+    '''
+    for i in range(1, len(A)):
+        key = A[i]
+        j = i
+        while(j > 0 and key < A[j - 1]):
+            A[j] = A[j - 1]
+            j -= 1
+        A[j] = key
+    return A
 
-def insertion_sort(arr):
-    pass
+
+my_arr = [31, 12, 2, 25, 17]
+
+print(insertion_sort(my_arr))
